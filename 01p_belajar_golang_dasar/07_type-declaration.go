6db4de761c00dafaaf99c294fabc3f66b0049305
suntree99 <suntree99@ymail.com>
2022-10-06 13:24:52 +0700

    update

---
+++ 01p_belajar_golang_dasar/07_type-declaration.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	type NoKTP string
+	type Married bool
+
+	var noKtpBudi NoKTP = "3603090710890001"
+	var marriedStatus Married = true
+	fmt.Println(noKtpBudi)
+	fmt.Println(marriedStatus)
+}
