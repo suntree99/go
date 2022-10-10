e11b175d6e598f157ff2dda69cacdeb72f1f4842
suntree99 <suntree99@ymail.com>
2022-10-10 23:51:10 +0700

    belajar-golang-goroutines selesai

---
+++ src/04p_belajar_golang_goroutines/channel_test.go
@@ -0,0 +1,23 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Budi Darmawan"
+		fmt.Println("Selesai mengirim data")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
